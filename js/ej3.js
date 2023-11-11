070019da14dbd65dde636ee0c8960e687e889e76
bruno.alarcon <alarconbruno14@gmail.com>
2023-11-10 21:27:54 -0300

    agrego ejercicios realizados, solo falta el 3

--- js/ej3.js
+++ js/ej3.js
@@ -18,37 +18,3 @@ console.log(resultados.length)
 
 
 // Función para simular el lanzamiento de un dado
-function lanzarDado() {
-    return Math.ceil(Math.random() * 6);
-  }
-  
-  // Función para simular el lanzamiento de dos dados y contar las sumas
-  function simularLanzamientos() {
-    var resultados = [];
-    var conteoApariciones = new Array(12).fill(0); // Inicializa un array de 12 elementos (para sumas de 2 a 12) con 0
-  
-    for (var i = 0; i < 50; i++) {
-      var dado1 = lanzarDado();
-      var dado2 = lanzarDado();
-      var suma = dado1 + dado2;
-  
-      // Anota la suma en el array de resultados
-      resultados.push(suma);
-  
-      // Incrementa el contador de apariciones para la suma actual
-      conteoApariciones[suma - 2]++;
-    }
-  
-    return { resultados, conteoApariciones };
-  }
-  
-  // Realiza la simulación de lanzamientos
-  var { resultados, conteoApariciones } = simularLanzamientos();
-  
-  // Muestra los resultados y el conteo de apariciones
-  console.log("Resultados de la simulación de lanzamiento de dos dados:");
-  console.log(resultados);
-  
-  console.log("\nConteo de apariciones de cada resultado:");
-  console.log(conteoApariciones);
-  
